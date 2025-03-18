1af06e2b8f3a4f46718201744abca4299614e81c
jtlandis <jtlandis314@gmail.com>
2025-03-18 15:57:38 -0400

    added very brief data annotation

---
+++ R/data.r
@@ -0,0 +1,9 @@
+#' @title Data set raw
+#' @description
+#' Data as provided by [https://archive.ics.uci.edu/dataset/611/hierarchical+sales+data]
+"data_set_raw"
+
+#' @title Data set tidy
+#' @description
+#' Data reshaped to be more "tidy"
+"data_set_tidy"
