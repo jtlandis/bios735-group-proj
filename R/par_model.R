751a2dfc217131fbea4c0c9a125e21f3a0a485bc
jtlandis <jtlandis314@gmail.com>
2025-04-21 19:33:00 -0400

    hard coding bfgs and Hessian for new model. either gamma or beta can be zero length vector

--- R/par_model.R
+++ R/par_model.R
@@ -124,3 +124,140 @@ par_gradient <- function(y, x, beta, gamma, mu = 0, q = length(beta)) {
 }
 
 
+
+par_bfgs <- function(
+  y,
+  x,
+  beta = numeric(0),
+  gamma,
+  max_iter = 1000,
+  trace_mod = 0
+) {
+  q <- length(beta)
+  p <- length(gamma)
+
+  beta_slice <- seq_len(q)
+  gamma_slice <- seq_len(p) + q
+
+  ll <- function(par, y, x) {
+    beta <- par[beta_slice]
+    gamma <- par[gamma_slice]
+    par_loglik(y, x, beta, gamma, mu = 0, q = q)
+  }
+
+  grad <- function(par, y, x) {
+    beta <- par[beta_slice]
+    gamma <- par[gamma_slice]
+    par_gradient(y, x, beta, gamma, mu = 0, q = q)
+  }
+
+  general_bfgs(
+    par = c(beta, gamma),
+    f = ll,
+    g = grad,
+    y = y,
+    x = x,
+    H = par_hessian(y, x, beta, gamma) |> solve(),
+    max_iter = max_iter,
+    step_size = 0.1,
+    trace_mod = trace_mod
+  )
+}
+
+# utility function to shift some data
+# vector of length `size` by some ammount
+# n <= q.
+# shift of 0 returns model data
+shifter <- function(q, size) {
+  seq_ <- seq_len(size - q)
+  function(data, n) {
+    if (n > q) {
+      stop(sprintf("n must be less than or equal to %i", q))
+    }
+    data[seq_ + q - n]
+  }
+}
+
+par_hessian <- function(y, x, beta, gamma) {
+  q <- length(beta)
+  p <- length(gamma)
+  T <- length(y)
+
+  shift <- shifter(q, T)
+  y_lags_slice <- rev(seq_len(q) - 1L)
+  x_t <- x[(q + 1):T, , drop = FALSE]
+  m_t <- Auto_t <- numeric(T - q)
+  Linear_t <- if (p > 0) as.vector(exp(x_t %*% gamma)) else rep(1, T - q)
+
+  weight_ <- 1 - sum(beta)
+  for (t in seq_along(Auto_t)) {
+    y_lags_slice <- y_lags_slice + 1L
+    y_lags <- y[y_lags_slice]
+    Auto_t[t] <- sum(beta * y_lags)
+    m_t[t] <- Auto_t[t] + (weight_ * Linear_t[t])
+  }
+  # create Hessian
+  P <- p + q
+  H <- matrix(0, P, P)
+  param_types <- c(rep("beta", q), rep("gamma", p))
+  yt_mt <- shift(y, 0) / m_t
+  w <- yt_mt / m_t
+  for (i in seq_along(param_types)) {
+    i_block <- param_types[i]
+
+    for (j in i:P) {
+      j_block <- param_types[j]
+      block <- sprintf("%s_%s", i_block, j_block)
+      switch(
+        block,
+        beta_beta = {
+          H[i, j] <- H[j, i] <- -sum(
+            w * (shift(y, i) - Linear_t) * (shift(y, j) - Linear_t)
+          )
+        },
+        beta_gamma = {
+          gamma_j <- j - q
+          x_j <- x_t[, gamma_j, drop = FALSE]
+          H[i, j] <- H[j, i] <- -sum(
+            w *
+              (shift(y, i) - Linear_t) *
+              weight_ *
+              Linear_t *
+              x_j +
+              ((yt_mt - 1) * Linear_t * x_j)
+          )
+        },
+        gamma_beta = {
+          gamma_i <- i - q
+          x_i <- x_t[, gamma_i, drop = FALSE]
+          H[i, j] <- H[j, i] <- -sum(
+            w *
+              (shift(y, j) - Linear_t) *
+              weight_ *
+              Linear_t *
+              x_i +
+              ((yt_mt - 1) * Linear_t * x_i)
+          )
+        },
+        gamma_gamma = {
+          gamma_i <- i - q
+          x_i <- x_t[, gamma_i, drop = FALSE]
+          gamma_j <- j - q
+          x_j <- x_t[, gamma_j, drop = FALSE]
+          H[i, j] <- H[j, i] <- -sum(
+            x_i *
+              x_j *
+              (
+                w *
+                  (weight_^2) *
+                  (Linear_t^2) +
+                  (Linear_t * (yt_mt - 1) * weight_)
+              )
+          )
+        }
+      )
+    }
+  }
+
+  H
+}
