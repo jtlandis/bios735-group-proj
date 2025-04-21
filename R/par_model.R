9ff049aad561e7f4a849df3234d119c4f1aae1e3
jtlandis <jtlandis314@gmail.com>
2025-04-21 19:27:45 -0400

    editor blank lines?

--- R/par_model.R
+++ R/par_model.R
@@ -46,21 +46,21 @@ par_loglik <- function(y, x, beta, gamma, mu = 0, q = length(beta)) {
 fit_par_optim <- function(y, x, q = 1) {
   T <- length(y)
   p <- ncol(x)
-  
-  init <- c(rep(0.05, q), rep(0.01, p))  # beta + gamma
-  
+
+  init <- c(rep(0.05, q), rep(0.01, p)) # beta + gamma
+
   nll <- function(par) {
     beta <- par[1:q]
     gamma <- par[(q + 1):(q + p)]
     -par_loglik(y, x, beta, gamma, mu = 0, q = q)
   }
-  
+
   grad <- function(par) {
     beta <- par[1:q]
     gamma <- par[(q + 1):(q + p)]
     -par_gradient(y, x, beta, gamma, mu = 0, q = q)
   }
-  
+
   fit <- optim(
     par = init,
     fn = nll,
@@ -68,7 +68,7 @@ fit_par_optim <- function(y, x, q = 1) {
     method = "BFGS",
     control = list(maxit = 200)
   )
-  
+
   list(
     beta = fit$par[1:q],
     gamma = fit$par[(q + 1):(q + p)],
@@ -94,30 +94,30 @@ fit_par_optim <- function(y, x, q = 1) {
 par_gradient <- function(y, x, beta, gamma, mu = 0, q = length(beta)) {
   T <- length(y)
   p <- length(gamma)
-  
+
   grad_beta <- rep(0, q)
   grad_gamma <- rep(0, p)
-  
+
   for (t in (q + 1):T) {
     y_lags <- y[(t - 1):(t - q)]
     x_t <- x[t, ]
     linear_part <- mu + sum(x_t * gamma)
-    
+
     a_t <- sum(beta * y_lags)
     b_t <- (1 - sum(beta)) * exp(linear_part)
     m_t <- a_t + b_t
-    
+
     prefactor <- (y[t] / m_t) - 1
-    
+
     for (l in 1:q) {
       grad_beta[l] <- grad_beta[l] + prefactor * (y[t - l] - exp(linear_part))
     }
-    
+
     for (j in 1:p) {
       grad_gamma[j] <- grad_gamma[j] + prefactor * (1 - sum(beta)) * x_t[j] * exp(linear_part)
     }
   }
-  
+
   grad <- c(beta = grad_beta, gamma = grad_gamma)
   return(grad)
 }
